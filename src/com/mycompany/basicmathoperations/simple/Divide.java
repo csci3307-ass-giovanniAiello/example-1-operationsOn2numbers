2a41c35a5d8404a08af7a84382e87eeffb41e7ac
Giovanni Aiello <Gio@student-wireless-m-1114.fdu.edu>
2017-04-05 18:07:02 -0400

    Second Commit

---
+++ src/com/mycompany/basicmathoperations/simple/Divide.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.mycompany.basicmathoperations.simple;
+
+/**
+ * @author Gio
+ *
+ */
+public class Divide {
+	public int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+}
