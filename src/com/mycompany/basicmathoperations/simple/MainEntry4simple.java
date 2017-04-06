88ce952de05efdbe6aa2ee36863bac6cee69ab58
Giovanni Aiello <Gio@Giovannis-Air.fios-router.home>
2017-04-06 09:24:43 -0400

    Third Commit

--- src/com/mycompany/basicmathoperations/simple/MainEntry4simple.java
+++ src/com/mycompany/basicmathoperations/simple/MainEntry4simple.java
@@ -28,18 +28,30 @@ public class MainEntry4simple {
 		number1 = scanner.nextInt();
 		System.out.println("Enter number 2");
 		number2 = scanner.nextInt();
-		System.out.println("Press 1 to Multiply");
 		
 		
 		// Multiply block
 		Multiply multiplier = new Multiply();
 		result = multiplier.multiply(number1, number2);
-		
+		System.out.printf("Multiply result is %.2f%n", result);
+		scanner.close();
 		// Divide block
 		Divide divider = new Divide();
 		result = divider.divide(number1, number2);
 		// print out result - NOTE %d is for int, %f is for float(and double), %.2f means 2 digits after decimal like 123.45
-		System.out.printf("result is %.2f%n", result);
+		System.out.printf("Divide result is %.2f%n", result);
+		scanner.close();
+		//Add block
+		Add adder = new Add();
+		result = adder.add(number1, number2);
+		// print out result - NOTE %d is for int, %f is for float(and double), %.2f means 2 digits after decimal like 123.45
+		System.out.printf("Add result is %.2f%n", result);
+		scanner.close();
+		//Subtract block
+		Subtract subtracter = new Subtract();
+		result = subtracter.subtract(number1, number2);
+		// print out result - NOTE %d is for int, %f is for float(and double), %.2f means 2 digits after decimal like 123.45
+		System.out.printf("Subtract result is %.2f%n", result);
 		scanner.close();
 	}
 
---
+++ src/com/mycompany/basicmathoperations/simple/Subtract.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.mycompany.basicmathoperations.simple;
+
+/**
+ * @author Gio
+ *
+ */
+public class Subtract {
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+}
